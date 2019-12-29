9182757f9e213911b5a765f617f295b3742843a7
Ibrahim shamma <i.abushammah@gmail.com>
2019-12-29 23:24:48 +0200

    Checking

--- server/routes/api/product.js
+++ server/routes/api/product.js
@@ -1,4 +1,13 @@
 var router = require('express').Router();
+var helper = require('../../../Middleware/helper');
+var { Routes } = require('../../../constants/constants');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+var Product = mongoose.model('Product');
 
+//TODO params + populate
+
+//SECTION add article
+router.post(Routes.addProduct, helper.required, function(req, res, next) {});
 
 module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
 var router = require('express').Router();
 
 router.use('/api', require('./api/user'));
+router.use('/api', require('./api/product'));
 
 module.exports = router;
